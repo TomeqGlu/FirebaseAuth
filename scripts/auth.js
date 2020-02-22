0916128c9d022b4fe7614aed3255017b77d92116
Tomeq <tomaszglu95@hotmail.com>
2020-02-22 18:54:22 +0100

    creating and signing up

---
+++ scripts/auth.js
@@ -0,0 +1,20 @@
+// sign up
+const singupForm = document.querySelector('#signup-form');
+singupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  //get user info
+  const email = singupForm['signup-email'].value;  
+  const password = singupForm['signup-password'].value;
+
+  console.log(email, password);
+
+  // sign up the user
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred);
+    const modal = document.querySelector('@modal-signup');
+    M.Modal.getInstance(modal).close();
+    singupForm.reset();
+  });
+  // line above will take smoe time to execute, that's why i added a promise
+})
